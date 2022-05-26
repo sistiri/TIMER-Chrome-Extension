ed0311430f6b0e70df4d94ec151e7e815e61de8a
sistiri <sistiri@yahoo.com>
2022-05-26 15:25:58 +0200

    Start, Stop & Reset Timer Features

--- popup.js
+++ popup.js
@@ -6,7 +6,7 @@ const timerElement = document.getElementById('timer')
 function updateTimeElements() {
   chrome.storage.local.get(["timer"], (res) => {
     const time = res.timer ?? 0
-    timerElement.textContent = `The timer i at: ${time} seconds`
+    timerElement.textContent = `The timer is at: ${time} seconds`
   })
   const currentTime = new Date().toLocaleTimeString()
   timeElement.textContent = `The local timer is: ${currentTime}`
@@ -24,4 +24,25 @@ setInterval(updateTimeElements, 1000)
 chrome.storage.sync.get(["name"], (res) => {
   const name = res.name ?? "???"
   nameElement.textContent = `Your name is: ${name}`
+})
+
+const startBtn = document.getElementById("start")
+const stopBtn = document.getElementById("stop")
+const resetBtn = document.getElementById("reset")
+
+startBtn.addEventListener("click", () => {
+  chrome.storage.local.set({
+    isRunning: true
+  })
+})
+stopBtn.addEventListener("click", () => {
+  chrome.storage.local.set({
+    isRunning: false
+  })
+})
+resetBtn.addEventListener("click", () => {
+  chrome.storage.local.set({
+    timer: 0,
+    isRunning: false
+  })
 })
